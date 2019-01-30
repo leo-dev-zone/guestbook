fc3af26e04cf7ad6b2e7e4c50ddae0169cb27280
leo-dev-zone <leoxavierdev@gmail.com>
2019-01-30 01:02:10 -0500

    Create master view

---
+++ app/Http/Controllers/SignaturesController.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class SignaturesController extends Controller
+{
+    public function index()
+    {
+        return view('signatures.index');
+    }
+}
